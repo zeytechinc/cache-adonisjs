5c8132be2ae56752a4751cb11ca2678ab63e505f
Steve Krenek <steve.krenek@zeytech.com>
2021-05-20 22:00:40 -0500

    Added new typings to index

--- adonis-typings/index.ts
+++ adonis-typings/index.ts
@@ -1,1 +1,5 @@
 /// <reference path="./cache.ts" />
+/// <reference path="./cache-item.ts" />
+/// <reference path="./health-check-helper.ts" />
+/// <reference path="./lru-cache.ts" />
+/// <reference path="./tlru-cache.ts" />
